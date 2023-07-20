b5c9041f4abecc9dba70affacc68051eee126a6f
Clayton Smith01~ <claytonsmith11@outlook.com>
2023-07-20 11:12:07 +0100

    Inital Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Variables, Constants and comments
+
+// let myAge = 25;
+// let year = 2023;
+
+// console.log(myAge, year)
+
+// MyAge = 30;
+// console.log(age);
+
+// const points = 100;
+// console.log(points);
+
+// var score = 75;
+// console.log(score)
+
+// Strings
+console.log('Hello World');
+let email = 'claytonsmith11@outlook.com';
+console.log(email);
+
+// String concatenation
+
+let firstName = 'Clayton';
+let lastName = 'Smith';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
