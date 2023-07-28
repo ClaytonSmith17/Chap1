b1eed08da2746161b619e93c041649a24963b107
Clayton Smith01~ <claytonsmith11@outlook.com>
2023-07-28 11:27:04 +0100

    updated

--- script.js
+++ script.js
@@ -568,3 +568,16 @@
 // console.log(userOne, userTwo);
 
 //DOM Explained
+//get an element by id
+// const title = document.getElementById('page-title');
+// console.log(title)
+
+// get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+//get element by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
